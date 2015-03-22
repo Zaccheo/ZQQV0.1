c08f59f511119e3b59e29a8e9837c7264c481fa3
yangbobo <306713329@qq.com>
2015-03-22 19:32:41 +0800

    将已有粉丝导入【用户信息表】

--- lanewechat/test/test.php
+++ lanewechat/test/test.php
@@ -5,10 +5,26 @@ use LaneWeChat\servers\users\UserRegister;
 include '../lanewechat.php';
 
 //获取用户的微信信息，并添加到用户信息数据库表中.
-$user = UserManage::getUserInfo("o5896s7PEwwnqM67-aeRKrdLLdFk");
+//$user = UserManage::getUserInfo("o5896s7PEwwnqM67-aeRKrdLLdFk");
 //echo $user['openid'];
 //echo $user['nickname'];
-echo $user['headimgurl'];
- echo UserRegister::unsubscribe($user['openid']);
+//echo $user['headimgurl'];
+ //echo UserRegister::unsubscribe($user['openid']);
+//==============================================================================================
+//将已有粉丝信息导入[用户信息表]
+/*
+$list = UserManage::getFansList();
+foreach ($list['data']['openid'] as $openid)
+{
+    $user = UserManage::getUserInfo($openid);
+    UserRegister::register($user['openid'],$user['nickname'],$user['headimgurl']);
+    echo $openid."<br/>";
+}
+*/
+//==============================================================================================
+// $openid = "";
+
+// $user = UserManage::getUserInfo($openid);
+// UserRegister::register($user['openid'],$user['nickname'],$user['headimgurl']);
 
 ?>
